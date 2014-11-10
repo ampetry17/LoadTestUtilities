850ec303c7ac093cff3a6753ea5ade197428e43b
fishg92 <gfish@teamnorthwoods.com>
2014-11-10 09:15:10 -0500

    Allow multiple saves

--- DocumentLoadTest/Form1.Designer.cs
+++ DocumentLoadTest/Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.txtOBPassword = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.lblCopies = new System.Windows.Forms.Label();
+            this.txtCopies = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // lblWorking
@@ -47,7 +49,7 @@
             this.lblWorking.Location = new System.Drawing.Point(203, 260);
             this.lblWorking.Name = "lblWorking";
             this.lblWorking.Size = new System.Drawing.Size(74, 20);
-            this.lblWorking.TabIndex = 5;
+            this.lblWorking.TabIndex = 11;
             this.lblWorking.Text = "Working";
             this.lblWorking.Visible = false;
             // 
@@ -57,7 +59,7 @@
             this.label1.Location = new System.Drawing.Point(49, 69);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(37, 13);
-            this.label1.TabIndex = 0;
+            this.label1.TabIndex = 4;
             this.label1.Text = "Pages";
             // 
             // cboPages
@@ -73,7 +75,7 @@
             this.cboPages.Location = new System.Drawing.Point(92, 66);
             this.cboPages.Name = "cboPages";
             this.cboPages.Size = new System.Drawing.Size(121, 21);
-            this.cboPages.TabIndex = 1;
+            this.cboPages.TabIndex = 5;
             this.cboPages.SelectedIndexChanged += new System.EventHandler(this.cboPages_SelectedIndexChanged);
             // 
             // rbBW
@@ -83,7 +85,7 @@
             this.rbBW.Location = new System.Drawing.Point(297, 64);
             this.rbBW.Name = "rbBW";
             this.rbBW.Size = new System.Drawing.Size(104, 17);
-            this.rbBW.TabIndex = 2;
+            this.rbBW.TabIndex = 6;
             this.rbBW.TabStop = true;
             this.rbBW.Text = "Black and White";
             this.rbBW.UseVisualStyleBackColor = true;
@@ -94,7 +96,7 @@
             this.rbColor.Location = new System.Drawing.Point(297, 87);
             this.rbColor.Name = "rbColor";
             this.rbColor.Size = new System.Drawing.Size(49, 17);
-            this.rbColor.TabIndex = 3;
+            this.rbColor.TabIndex = 7;
             this.rbColor.Text = "Color";
             this.rbColor.UseVisualStyleBackColor = true;
             // 
@@ -104,7 +106,7 @@
             this.btnScan.Location = new System.Drawing.Point(202, 153);
             this.btnScan.Name = "btnScan";
             this.btnScan.Size = new System.Drawing.Size(75, 23);
-            this.btnScan.TabIndex = 4;
+            this.btnScan.TabIndex = 10;
             this.btnScan.Text = "Scan";
             this.btnScan.UseVisualStyleBackColor = true;
             this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
@@ -114,7 +116,7 @@
             this.txtOBUsername.Location = new System.Drawing.Point(113, 12);
             this.txtOBUsername.Name = "txtOBUsername";
             this.txtOBUsername.Size = new System.Drawing.Size(100, 20);
-            this.txtOBUsername.TabIndex = 6;
+            this.txtOBUsername.TabIndex = 1;
             // 
             // txtOBPassword
             // 
@@ -122,7 +124,7 @@
             this.txtOBPassword.Name = "txtOBPassword";
             this.txtOBPassword.PasswordChar = '*';
             this.txtOBPassword.Size = new System.Drawing.Size(100, 20);
-            this.txtOBPassword.TabIndex = 7;
+            this.txtOBPassword.TabIndex = 3;
             // 
             // label2
             // 
@@ -130,7 +132,7 @@
             this.label2.Location = new System.Drawing.Point(34, 15);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(73, 13);
-            this.label2.TabIndex = 8;
+            this.label2.TabIndex = 0;
             this.label2.Text = "OB Username";
             // 
             // label3
@@ -139,14 +141,34 @@
             this.label3.Location = new System.Drawing.Point(254, 15);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(71, 13);
-            this.label3.TabIndex = 9;
+            this.label3.TabIndex = 2;
             this.label3.Text = "OB Password";
+            // 
+            // lblCopies
+            // 
+            this.lblCopies.AutoSize = true;
+            this.lblCopies.Location = new System.Drawing.Point(47, 109);
+            this.lblCopies.Name = "lblCopies";
+            this.lblCopies.Size = new System.Drawing.Size(39, 13);
+            this.lblCopies.TabIndex = 8;
+            this.lblCopies.Text = "Copies";
+            // 
+            // txtCopies
+            // 
+            this.txtCopies.Location = new System.Drawing.Point(92, 106);
+            this.txtCopies.Name = "txtCopies";
+            this.txtCopies.Size = new System.Drawing.Size(100, 20);
+            this.txtCopies.TabIndex = 9;
+            this.txtCopies.Text = "1";
+            this.txtCopies.Enter += new System.EventHandler(this.txtCopies_Enter);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(507, 318);
+            this.Controls.Add(this.txtCopies);
+            this.Controls.Add(this.lblCopies);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtOBPassword);
@@ -159,6 +181,7 @@
             this.Controls.Add(this.lblWorking);
             this.Name = "Form1";
             this.Text = "Compass Documents Load Testing";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,6 +199,8 @@
         private System.Windows.Forms.TextBox txtOBPassword;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCopies;
+        private System.Windows.Forms.TextBox txtCopies;
     }
 }
 
--- DocumentLoadTest/Form1.cs
+++ DocumentLoadTest/Form1.cs
@@ -33,8 +33,6 @@ namespace DocumentLoadTest
 
         private void SaveDocument(string FileName)
         {
-            lblWorking.Visible = true;
-            Application.DoEvents();
 
             Stopwatch sw = Stopwatch.StartNew();
 
@@ -121,7 +119,6 @@ namespace DocumentLoadTest
             string testFile = System.Configuration.ConfigurationManager.AppSettings["TestFileFolder"] + @"\" + FileName;
             byte[] documentAsBytes = System.IO.File.ReadAllBytes(testFile);
 
-            ChunkingUploader chunkUploader = new ChunkingUploader();
 
             ICompassDocumentsService svc = null;
             cDocument oRet = null;
@@ -162,7 +159,6 @@ namespace DocumentLoadTest
             LogResults(sw.ElapsedMilliseconds);
             sw.Stop();
 
-            lblWorking.Visible = false;
         }
 
         private void LogResults(long ms)
@@ -277,12 +273,6 @@ namespace DocumentLoadTest
 
             return DataInterface.DBInterface.ScalarString(sql, string.Empty);
         }
-
-         private void btn1bw_Click(object sender, EventArgs e)
-        {
-            SaveDocument("1bw.tif");
-        }
-
         private DmsEntityTypes GetEntityTypes()
         {
             if (_entityTypes != null) return _entityTypes;
@@ -299,11 +289,6 @@ namespace DocumentLoadTest
         private List<cKeywordType> GetBlankKeywordTypes()
         {
             return GetEntityTypes().KeywordTypes;
-        }
-
-        private void btn2bw_Click(object sender, EventArgs e)
-        {
-            SaveDocument("2bw.tif");
         }
 
         private void btnScan_Click(object sender, EventArgs e)
@@ -342,12 +327,32 @@ namespace DocumentLoadTest
             }
             fileName += ".tif";
 
-            SaveDocument(fileName);
+            Color originalColor = this.BackColor;
+
+            lblWorking.Visible = true;
+            this.BackColor = Color.Red;
+            Application.DoEvents();
+
+            int copies = int.Parse(txtCopies.Text);
+
+            for (int i = 1; i <= copies; i++)
+            {
+                SaveDocument(fileName);
+            }
+
+            lblWorking.Visible = false;
+            this.BackColor = originalColor;
+
         }
 
         private void cboPages_SelectedIndexChanged(object sender, EventArgs e)
         {
             btnScan.Enabled = (cboPages.SelectedIndex != -1);
+        }
+
+        private void txtCopies_Enter(object sender, EventArgs e)
+        {
+            txtCopies.SelectAll();
         }
     }
 }
